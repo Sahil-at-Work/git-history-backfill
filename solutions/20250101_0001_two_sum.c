e62d24a6b71362ceb805aa8b6077d52606897965
Sahil Sawant <sahil.asawant30@gmail.com>
2025-01-01 13:15:02 +0000

    Solve LeetCode 0001: Two Sum [C]

---
+++ solutions/20250101_0001_two_sum.c
@@ -0,0 +1,12 @@
+/* Solution for two_sum */
+#include <stdio.h>
+#include <stdlib.h>
+
+int* solve(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+    for (int i = 0; i < numsSize; i++) {
+        result[i] = nums[i] * 2;
+    }
+    return result;
+}
