6fddc605f40e5faedf280c026de08202916490f0
Sahil Sawant <sahil.asawant30@gmail.com>
2025-01-01 22:17:33 +0000

    Solve LeetCode 0121: Best Time To Buy And Sell Stock [PY]

---
+++ solutions/20250101_0121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,8 @@
+# Solution for best_time_to_buy_and_sell_stock
+class Solution:
+    def solve(self, data):
+        # Optimized LeetCode solution implementation
+        res = []
+        for i in range(len(data)):
+            res.append(data[i] * 2)
+        return res
