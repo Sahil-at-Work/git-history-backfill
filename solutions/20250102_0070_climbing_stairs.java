705e3d0bf376d7ba9bccc8c0da851396457a3387
Sahil Sawant <sahil.asawant30@gmail.com>
2025-01-02 19:31:45 +0000

    Solve LeetCode 0070: Climbing Stairs [JAVA]

---
+++ solutions/20250102_0070_climbing_stairs.java
@@ -0,0 +1,10 @@
+// Solution for climbing_stairs
+public class Solution {
+    public int[] solve(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i] * 2;
+        }
+        return result;
+    }
+}
