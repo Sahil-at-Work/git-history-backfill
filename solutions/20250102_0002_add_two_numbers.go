a64202b0ac3b73da512bacaf3acf8c0e5146b184
Sahil Sawant <sahil.asawant30@gmail.com>
2025-01-02 19:30:08 +0000

    Solve LeetCode 0002: Add Two Numbers [GO]

---
+++ solutions/20250102_0002_add_two_numbers.go
@@ -0,0 +1,10 @@
+// Solution for add_two_numbers
+package main
+
+func solve(nums []int) []int {
+    result := make([]int, len(nums))
+    for i, v := range nums {
+        result[i] = v * 2
+    }
+    return result
+}
