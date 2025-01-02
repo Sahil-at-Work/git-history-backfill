de4c672fb1f6756e2da8c3132ef5734c6a2362ba
Sahil Sawant <sahil.asawant30@gmail.com>
2025-01-02 22:40:41 +0000

    Solve LeetCode 0021: Merge Two Sorted Lists [JAVA]

---
+++ solutions/20250102_0021_merge_two_sorted_lists.java
@@ -0,0 +1,10 @@
+// Solution for merge_two_sorted_lists
+public class Solution {
+    public int[] solve(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = nums[i] * 2;
+        }
+        return result;
+    }
+}
