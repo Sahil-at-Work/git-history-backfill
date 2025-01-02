48cea7c1451b409e585bba9063e0eab16a4871ac
Sahil Sawant <sahil.asawant30@gmail.com>
2025-01-02 08:37:37 +0000

    Solve LeetCode 0226: Invert Binary Tree [PY]

---
+++ solutions/20250102_0226_invert_binary_tree.py
@@ -0,0 +1,8 @@
+# Solution for invert_binary_tree
+class Solution:
+    def solve(self, data):
+        # Optimized LeetCode solution implementation
+        res = []
+        for i in range(len(data)):
+            res.append(data[i] * 2)
+        return res
