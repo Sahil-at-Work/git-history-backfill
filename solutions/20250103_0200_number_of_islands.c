a06b8170ee92214b2a438ffd1f2c51577edf2b71
Sahil Sawant <sahil.asawant30@gmail.com>
2025-01-03 20:32:51 +0000

    Solve LeetCode 0200: Number Of Islands [C]

---
+++ solutions/20250103_0200_number_of_islands.c
@@ -0,0 +1,12 @@
+/* Solution for number_of_islands */
+#include <stdio.h>
+#include <stdlib.h>
+
+int* solve(int* nums, int numsSize, int* returnSize) {
+    int* result = (int*)malloc(numsSize * sizeof(int));
+    *returnSize = numsSize;
+    for (int i = 0; i < numsSize; i++) {
+        result[i] = nums[i] * 2;
+    }
+    return result;
+}
